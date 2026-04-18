f4dd84130179efccb43324eb28ef305d6644e264
Gabriel_dos_Santos_Torres <gabrielstorres00@gmail.com>
2026-04-18 10:30:46 -0300

    first commit

---
+++ src/main/java/com/example/first_aplication_spring/FirstAplicationSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_aplication_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAplicationSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstAplicationSpringApplication.class, args);
+	}
+
+}
